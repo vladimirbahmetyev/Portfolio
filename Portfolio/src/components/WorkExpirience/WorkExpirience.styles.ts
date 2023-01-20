e5e0c648d43a6d385cc2f3720e8581b111f946cd
Vladimir <bahv-240298@mail.ru>
2023-01-20 19:29:45 +0400

    Add simple native navigation

--- Portfolio/src/components/WorkExpirience/WorkExpirience.styles.ts
+++ Portfolio/src/components/WorkExpirience/WorkExpirience.styles.ts
@@ -32,13 +32,15 @@ export const useStyle = makeStyles()(()=> ({
 		top: -66
 	},
 	projectContainer:{
+		position: "relative",
 		display: "flex",
 		svg:{
 			marginTop: 80,
 		}
 	},
 	projectPack:{
-		mixBlendMode: "lighten"
+		mixBlendMode: "lighten",
+		zIndex: 2,
 	},
 	firstSecondProject:{
 		display: "flex",
@@ -48,31 +50,83 @@ export const useStyle = makeStyles()(()=> ({
 			marginLeft: 250,
 			marginTop: 250,
 		},
-		"div:last-of-type":{
-			img:{
-				height: 520,
-				width: 617
-			}
+	},
+	firstProject:{
+		"img:first-of-type":{
+			width: 617,
+			height: 520,
+		},
+		"img:last-of-type":{
+			width: 560,
+			height: 300,
+			right: 28,
+			top: 100
+		}
+	},
+	secondProject:{
+		"img:first-of-type":{
+			width: 403,
+			height: 348,
+		},
+		"img:last-of-type":{
+			width: 300,
+			height: 300,
+			right: 50,
+			top: 25
 		}
 	},
 	thirdProject:{
 		justifyContent:"flex-end",
 		marginTop: 30,
+		"img:last-of-type":{
+			width: 760,
+			height: 450,
+			right: 25,
+			top: 55
+		}
 	},
 	fourthProject:{
 		marginTop: 30,
 		marginLeft: 120,
+		"img:last-of-type":{
+			width: 400,
+			height: 300,
+			left: 110,
+			top: 25
+		}
 	},
 	fifthProject:{
 		justifyContent:"flex-end",
 		marginTop: -70,
+		"img:last-of-type":{
+			width: 560,
+			height: 300,
+			right: 30,
+			top: 95
+		}
 	},
 	sixthProject:{
 		marginTop: -270,
-		paddingLeft: 250
+		paddingLeft: 250,
+		"img:last-of-type":{
+			width: 300,
+			height: 300,
+			left: 340,
+			top: 30
+		}
 	},
 	seventhProject:{
 		justifyContent: "flex-end",
-		marginTop: 60
+		marginTop: 60,
+		"img:last-of-type":{
+			width: 760,
+			height: 450,
+			right: 30,
+			top: 35
+		}
+	},
+	projectImage:{
+		position: "absolute",
+		zIndex: 1,
 	}
 }));
--- Portfolio/src/components/WorkExpirience/WorkExpirience.tsx
+++ Portfolio/src/components/WorkExpirience/WorkExpirience.tsx
@@ -6,7 +6,7 @@ import {Typography} from "ui-kit";
 export const WorkExpirience:FC= () => {
 	const {classes, cx} = useStyle();
 	return (
-		<div className={classes.workExperienceContainer}>
+		<div className={classes.workExperienceContainer} id='work'>
 			<img src='/images/expTap.png' className={classes.img}/>
 			<div className={classes.titleContainer}>
 				<div className={classes.rowContainer}>
@@ -19,34 +19,41 @@ export const WorkExpirience:FC= () => {
 				<ExperienceTitle className={classes.experience}/>
 			</div>
 			<div className={classes.firstSecondProject}>
-				<div className={classes.projectContainer}>
+				<div className={cx(classes.projectContainer,classes.secondProject)}>
 					<N2/>
 					<img src='/images/workExperience/p2.png' className={classes.projectPack}/>
+					<img src='/images/workExperience/pr2.jpg' className={classes.projectImage}/>
 				</div>
-				<div className={classes.projectContainer}>
+				<div className={cx(classes.projectContainer, classes.firstProject)}>
 					<N1/>
 					<img src='/images/workExperience/p1.png' className={classes.projectPack}/>
+					<img src='/images/workExperience/pr1.jpg'className={classes.projectImage}/>
 				</div>
 			</div>
 			<div className={cx(classes.projectContainer, classes.thirdProject)}>
 				<N3/>
 				<img src='/images/workExperience/p3.png' className={classes.projectPack}/>
+				<img src='/images/workExperience/pr3.jpg'className={classes.projectImage}/>
 			</div>
 			<div className={cx(classes.projectContainer, classes.fourthProject)}>
 				<N4/>
 				<img src='/images/workExperience/p4.png' className={classes.projectPack}/>
+				<img src='/images/workExperience/pr4.jpg'className={classes.projectImage}/>
 			</div>
 			<div className={cx(classes.projectContainer, classes.fifthProject)}>
 				<N5/>
 				<img src='/images/workExperience/p5.png' className={classes.projectPack}/>
+				<img src='/images/workExperience/pr5.jpg'className={classes.projectImage}/>
 			</div>
 			<div className={cx(classes.projectContainer, classes.sixthProject)}>
 				<N6/>
 				<img src='/images/workExperience/p6.png' className={classes.projectPack}/>
+				<img src='/images/workExperience/pr6.jpg'className={classes.projectImage}/>
 			</div>
 			<div className={cx(classes.projectContainer, classes.seventhProject)}>
 				<N7/>
 				<img src='/images/workExperience/p7.png' className={classes.projectPack}/>
+				<img src='/images/workExperience/pr7.jpg'className={classes.projectImage}/>
 			</div>
 		</div>);
 };
