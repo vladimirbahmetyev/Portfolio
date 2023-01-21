25cba4fdedd34fb283e5e4146284af1b822c91ec
Vladimir <bahv-240298@mail.ru>
2023-01-21 13:32:44 +0400

    Add new files

---
+++ Portfolio/src/Pages/Project/Project.styles.ts
@@ -0,0 +1,3 @@
+import {makeStyles} from "../../theme";
+
+export const useStyle = makeStyles()(()=>({}));
---
+++ Portfolio/src/Pages/Project/Project.tsx
@@ -0,0 +1,7 @@
+import {FC} from "react";
+import {useStyle} from "./Project.styles";
+
+export const Project: FC = () => {
+	const {classes} = useStyle();
+	return <div></div>;
+};
