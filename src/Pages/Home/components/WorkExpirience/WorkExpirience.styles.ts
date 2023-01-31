4f84431bf59fd9059847a8451ed42a3f43178ff1
Vladimir <bahv-240298@mail.ru>
2023-01-31 18:39:16 +0400

    Update projects layout for other projects

--- src/Pages/Home/components/WorkExpirience/WorkExpirience.styles.ts
+++ src/Pages/Home/components/WorkExpirience/WorkExpirience.styles.ts
@@ -166,301 +166,143 @@ export const useStyle = makeStyles()(()=> ({
 			width: "100%"
 		}
 	},
-	fourthProject:{
-		marginTop: 30,
-		marginLeft: 180,
-		div:{
-			position: "relative",
-			minHeight: 400,
-			":hover":{
-				"img:first-of-type":{
-					top: 75,
-					transitionDuration: "300ms"
-				},
-				"img:last-of-type":{
-					top: 0,
-					transitionDuration: "300ms"
-				},
-				span:{
-					top: 315,
-					transitionDuration: "300ms"
-				}
-			}
-		},
-		"img:first-of-type":{
-			minWidth: 400,
-			minHeight: 300,
-			top: 25,
-			left: 0,
-			zIndex: 2,
-			position: "absolute",
-			transitionDuration: "300ms"
-		},
-		"img:last-of-type":{
-			width: 400,
-			height: 300,
-			left: 40,
-			top: 50,
-			zIndex: 1,
-			position: "absolute",transitionDuration: "300ms"
-		},
-		span:{
-			left: 40,
-			top: 365,
-			transitionDuration: "300ms",
-			position: "absolute"
+	fourthProjectContainer:{
+		display: "flex",
+		justifyContent: "flex-start",
+		marginTop: 100,
+		svg:{
+			marginTop: 80,
 		},
 		"@media (max-width: 820px)":{
-			margin: 0,
-			width: "100%",
-			div:{
-				minHeight: 250
-			},
-			"img:first-of-type":{
-				minWidth: 0,
-				minHeight: 0,
-				width:"105%",
-				left: -10,
-				top: 0,
-				position: "absolute",
-				transitionDuration: "300ms",
-			},
-			"img:last-of-type":{
-				width:"80%",
-				height: "auto",
-				minWidth: 0,
-				minHeight: 0,
-				left: 10,
-				top: 40,
-				transitionDuration: "300ms",
-			},
-			span: {
-				position: "absolute",
-				left: 170,
-				top: -35,
-				transitionDuration: "300ms",
-			},
-		}
-	},
-	fifthProject:{
-		div:{
-			position: "relative",
-			minHeight: 400,
-			":hover":{
-				"img:first-of-type":{
-					top: 50,
-					transitionDuration: "300ms",
-				},
-				"img:last-of-type":{
-					top: 40,
-					transitionDuration: "300ms"
-				},
-				span:{
-					top: 355,
-					transitionDuration: "300ms"
-				}
+			flexDirection: "column",
+			padding: 0,
+			marginTop: 50,
+			svg:{
+				marginTop: 0
 			}
 		},
-		justifyContent:"center",
-		marginLeft: 250,
-		marginTop: -40,
-		"img:last-of-type":{
-			minWidth: 560,
-			minHeight: 300,
+		span: {
 			position: "absolute",
-			top: 90,
-			left: 30,
-			transitionDuration: "300ms"
+			left: "5%",
+			bottom: "5%"
+		}
+
+	},
+	fourthProjectImage:{
+		position: "absolute",
+		top: "5%",
+		left: "5%",
+		width: "80%",
+		"@media (max-width: 820px)":{
+			width: "75%"
+		}
+	},
+	fourthProjectPack:{
+		width:"auto",
+		zIndex: 1,
+		"@media (max-width: 820px)":{
+			width: "100%"
+		}
+	},
+	fifthProjectContainer:{
+		display: "flex",
+		justifyContent:"flex-end",
+		svg:{
+			marginTop: 80,
 		},
-		"img:first-of-type":{
+		"@media (max-width: 820px)":{
+			flexDirection: "column",
+			svg:{
+				marginTop: 0
+			}
+		},
+		span: {
 			position: "absolute",
+			left: "5%",
+			bottom: "15%"
+		}
+
+	},
+	fifthProjectImage:{
+		position: "absolute",
+		top: "20%",
+		left: "5%",
+		width: "90%",
+	},
+	fifthProjectPack:{
+		width:"auto",
+		"@media (max-width: 820px)":{
+			width: "100%"
+		}
+	},
+	sixthProjectContainer:{
+		position: "relative",
+		paddingLeft:"20%",
+		top: -320,
+		span: {
+			position: "absolute",
+			left: "1%",
+			bottom: "5%"
+		},
+		"@media (max-width: 820px)":{
 			top: 0,
-			transitionDuration: "300ms"
-		},
-		span: {
-			position: "absolute",
-			transitionDuration: "300ms",
-			left: 30,
-			top: 405,
-		},
-		"@media (max-width: 820px)":{
-			margin: 0,
-			width: "100%",
-			div:{
-				minHeight: 250
-			},
-			"img:first-of-type":{
-				minWidth: 0,
-				minHeight: 0,
-				width:"105%",
-				left: -10,
-				top: 0,
-				position: "absolute",
-				transitionDuration: "300ms",
-			},
-			"img:last-of-type":{
-				width:"94%",
-				height: "auto",
-				minWidth: 0,
-				minHeight: 0,
-				left: 10,
-				top: 60,
-				transitionDuration: "300ms",
-			},
-			span: {
-				position: "absolute",
-				left: 70,
-				top: -35,
-				transitionDuration: "300ms",
-			},
+			padding: 0,
+			maxWidth: "100%",
 		}
 	},
-	sixthProject:{
-		marginTop: -140,
-		justifyContent:"center",
-		marginRight: 700,
-		minHeight: 400,
-		div:{
-			position: "relative",
-			":hover":{
-				"img:last-of-type":{
-					top: -20,
-					transitionDuration: "300ms",
-				},
-				"img:first-of-type":{
-					top: 50,
-					transitionDuration: "300ms",
-				},
-				span:{
-					transitionDuration: "300ms",
-					top: 295
-				}
-			}
-		},
-
-		"img:last-of-type":{
-			width: 300,
-			height: 300,
-			position: "absolute",
-			transitionDuration: "300ms",
-			left: 20,
-			top: 30,
-		},
-		"img:first-of-type":{
-			position: "absolute",
-			transitionDuration: "300ms",
-			top: 0
-		},
-		span: {
-			top: 345,
-			position: "absolute",
-			left: 20,
-			transitionDuration: "300ms",
-		},
-		"@media (max-width: 820px)":{
-			margin: 0,
-			width: "100%",
-			div:{
-				minHeight: 250
-			},
-			"img:first-of-type":{
-				minWidth: 0,
-				minHeight: 0,
-				width:"105%",
-				left: -10,
-				top: 0,
-				position: "absolute",
-				transitionDuration: "300ms",
-			},
-			"img:last-of-type":{
-				width:"100%",
-				height: "auto",
-				minWidth: 0,
-				minHeight: 0,
-				left: 0,
-				top: 40,
-				transitionDuration: "300ms",
-			},
-			span: {
-				position: "absolute",
-				left: 170,
-				top: -35,
-				transitionDuration: "300ms",
-			},
+	sixthProjectImage:{
+		position: "absolute",
+		top: "10%",
+		left: "1%",
+		width: "90%",
+		maxWidth: 310,
+		"@media (max-width:820px)":{
+			left:"5%",
+			maxWidth: "90%"
 		}
 	},
-	seventhProject:{
+	sixthProjectPack:{
+		"@media (max-width:820px)":{
+			width: "100%"
+		}
+	},
+	seventhProjectContainer:{
+		display: "flex",
 		justifyContent: "center",
-		marginTop: 60,
-		minHeight: 800,
-		marginLeft: -100,
-		div:{
-			position: "relative",
-			":hover":{
-				"img:last-of-type":{
-					transitionDuration: "300ms",
-					left: 75,
-				},
-				"img:first-of-type":{
-					transitionDuration: "300ms",
-					left: -50
-				},
-				span:{
-					transitionDuration: "300ms",
-					left: 75,
-				}
-			}
-		},
-		"img:last-of-type":{
-			width: 760,
-			height: 450,
-			position: "absolute",
-			top: 40,
-			left: 25,
-			transitionDuration: "300ms",
-
-		},
-		"img:first-of-type":{
-			position: "absolute",
-			transitionDuration: "300ms",
-			left: 0,
-		},
-		span:{
-			position: "absolute",
-			transitionDuration: "300ms",
-			top: 505,
-			left: 25
+		paddingLeft: "20%",
+		marginTop: -250,
+		svg:{
+			marginTop: 80,
 		},
 		"@media (max-width: 820px)":{
-			margin: 0,
-			width: "100%",
-			div:{
-				minHeight: 100
-			},
-			"img:first-of-type":{
-				minWidth: 0,
-				minHeight: 0,
-				width:"105%",
-				left: -10,
-				top: 0,
-				position: "absolute",
-				transitionDuration: "300ms",
-			},
-			"img:last-of-type":{
-				width:"100%",
-				height: "auto",
-				minWidth: 0,
-				minHeight: 0,
-				left: 0,
-				top: 40,
-				transitionDuration: "300ms",
-			},
-			span: {
-				position: "absolute",
-				left: 70,
-				top: -35,
-				transitionDuration: "300ms",
-			},
+			flexDirection: "column",
+			padding: 0,
+			marginTop: 50,
+			svg:{
+				marginTop: 0
+			}
+		},
+		span: {
+			position: "absolute",
+			left: "5%",
+			bottom: "10%"
+		}
+
+	},
+	seventhProjectImage:{
+		position: "absolute",
+		top: "10%",
+		left: "5%",
+		width: "90%",
+		"@media (max-width: 820px)":{
+			width: "80%"
+		}
+	},
+	seventhProjectPack:{
+		width:"auto",
+		zIndex: 1,
+		"@media (max-width: 820px)":{
+			width: "100%"
 		}
 	},
 	projectImage:{
--- src/Pages/Home/components/WorkExpirience/WorkExpirience.tsx
+++ src/Pages/Home/components/WorkExpirience/WorkExpirience.tsx
@@ -33,16 +33,16 @@ export const WorkExpirience:FC= () => {
 				<div className={classes.secondProjectContainer} onClick={()=>onProjectClick("Jobswidget")}>
 					<N2/>
 					<div className={classes.projectContainer}>
-						<img src='/images/workExperience/p2.png' className={cx(classes.projectPack, classes.secondProjectPack)}/>
-						<img src='/images/workExperience/pr2.jpg' className={cx(classes.projectImage, classes.secondProjectImage)}/>
+						<img src='/images/workExperience/p2.png' className={classes.secondProjectPack}/>
+						<img src='/images/workExperience/pr2.jpg' className={ classes.secondProjectImage}/>
 						<Typography variant='h3' block>JobsWidget</Typography>
 					</div>
 				</div>
 				<div className={classes.firstProjectContainer  } onClick={()=>onProjectClick("Metamap")}>
 					<N1/>
 					<div className={classes.projectContainer}>
-						<img src='/images/workExperience/p1.png' className={cx(classes.projectPack, classes.firstProjectPack)}/>
-						<img src='/images/workExperience/pr1.jpg'className={cx(classes.projectImage, classes.firstProjectImage)}/>
+						<img src='/images/workExperience/p1.png' className={classes.firstProjectPack}/>
+						<img src='/images/workExperience/pr1.jpg'className={classes.firstProjectImage}/>
 						<Typography variant='h3' block>Metamap</Typography>
 					</div>
 				</div>
@@ -55,37 +55,37 @@ export const WorkExpirience:FC= () => {
 					<Typography variant='h3' block>Game Greed</Typography>
 				</div>
 			</div>
-			{/*<div className={cx(classes.projectContainer, classes.fourthProject)} onClick={()=>onProjectClick("Mugler")}>*/}
-			{/*	<N4/>*/}
-			{/*	<div>*/}
-			{/*		<img src='/images/workExperience/p4.png' className={classes.projectPack}/>*/}
-			{/*		<img src='/images/workExperience/pr4.jpg'className={classes.projectImage}/>*/}
-			{/*		<Typography variant='h3' block>Mugler</Typography>*/}
-			{/*	</div>*/}
-			{/*</div>*/}
-			{/*<div className={cx(classes.projectContainer, classes.fifthProject)} onClick={()=>onProjectClick("MyGlo")}>*/}
-			{/*	<N5/>*/}
-			{/*	<div>*/}
-			{/*		<img src='/images/workExperience/p5.png' className={classes.projectPack}/>*/}
-			{/*		<img src='/images/workExperience/pr5.jpg'className={classes.projectImage}/>*/}
-			{/*		<Typography variant='h3' block>MyGlo</Typography>*/}
-			{/*	</div>*/}
-			{/*</div>*/}
-			{/*<div className={cx(classes.projectContainer, classes.sixthProject)} onClick={()=>onProjectClick("CorpPass")}>*/}
-			{/*	<N6/>*/}
-			{/*	<div>*/}
-			{/*		<img src='/images/workExperience/p6.png' className={classes.projectPack}/>*/}
-			{/*		<img src='/images/workExperience/pr6.jpg'className={classes.projectImage}/>*/}
-			{/*		<Typography variant='h3' block>CorpPass</Typography>*/}
-			{/*	</div>*/}
-			{/*</div>*/}
-			{/*<div className={cx(classes.projectContainer, classes.seventhProject)} onClick={()=>onProjectClick("Shopic")}>*/}
-			{/*	<N7/>*/}
-			{/*	<div>*/}
-			{/*		<img src='/images/workExperience/p7.png' className={classes.projectPack}/>*/}
-			{/*		<img src='/images/workExperience/pr7.jpg'className={classes.projectImage}/>*/}
-			{/*		<Typography variant='h3' block>Shopic</Typography>*/}
-			{/*	</div>*/}
-			{/*</div>*/}
+			<div className={classes.fourthProjectContainer} onClick={()=>onProjectClick("Mugler")}>
+				<N4/>
+				<div className={classes.projectContainer}>
+					<img src='/images/workExperience/p4.png' className={classes.fourthProjectPack}/>
+					<img src='/images/workExperience/pr4.jpg'className={classes.fourthProjectImage}/>
+					<Typography variant='h3' block>Mugler</Typography>
+				</div>
+			</div>
+			<div className={classes.fifthProjectContainer} onClick={()=>onProjectClick("MyGlo")}>
+				<N5/>
+				<div className={classes.projectContainer}>
+					<img src='/images/workExperience/p5.png' className={classes.fifthProjectPack}/>
+					<img src='/images/workExperience/pr5.jpg'className={classes.fifthProjectImage}/>
+					<Typography variant='h3' block>MyGlo</Typography>
+				</div>
+			</div>
+			<div className={classes.sixthProjectContainer} onClick={()=>onProjectClick("CorpPass")}>
+				<N6/>
+				<div className={classes.projectContainer}>
+					<img src='/images/workExperience/p6.png' className={classes.sixthProjectPack}/>
+					<img src='/images/workExperience/pr6.jpg'className={classes.sixthProjectImage}/>
+					<Typography variant='h3' block>CorpPass</Typography>
+				</div>
+			</div>
+			<div className={classes.seventhProjectContainer} onClick={()=>onProjectClick("Shopic")}>
+				<N7/>
+				<div className={classes.projectContainer}>
+					<img src='/images/workExperience/p7.png' className={classes.seventhProjectPack}/>
+					<img src='/images/workExperience/pr7.jpg'className={classes.seventhProjectImage}/>
+					<Typography variant='h3' block>Shopic</Typography>
+				</div>
+			</div>
 		</div>);
 };
