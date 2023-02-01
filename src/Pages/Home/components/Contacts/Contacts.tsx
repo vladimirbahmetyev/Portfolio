e34abb3880546f823700d3df9311b29d44b66233
Vladimir <bahv-240298@mail.ru>
2023-02-01 18:15:36 +0400

    Add new classes to mobile layout

--- src/Pages/Home/components/Contacts/Contacts.tsx
+++ src/Pages/Home/components/Contacts/Contacts.tsx
@@ -3,16 +3,31 @@ import {useStyle} from "./Contacts.styles";
 import {ContactsTitle, HH, Instagram, LinkedIn, Telegram} from "icons";
 import {Button, Input, Typography} from "ui-kit";
 import {Track, TrackMobile} from "./Assets";
-import {useScreenWidth} from "../../../../shared";
+import {useScreenWidth} from "shared";
+
+type FocusElementType = "name" | "email" | "message" | ""
 
 export const Contacts:FC = () => {
-	const [focus, setFocus] = useState<string>("");
+	const [focus, setFocus] = useState<FocusElementType>("");
 	const [name, setName] = useState<string>("");
 	const [email, setEmail] = useState<string>("");
 	const [message, setMessage] = useState<string>("");
 	const [isButtonFocus, setIsButtonFocus] = useState<boolean>(false);
 	const width = useScreenWidth();
-	const {classes, cx} =useStyle({screenWidth: width});
+	const {classes, cx} = useStyle({screenWidth: width});
+
+	const animationClasses: Record<FocusElementType, string> = {
+		name: classes.nameTrackFocus,
+		email: classes.emailFocus,
+		message: classes.messageFocus,
+		"":""
+	};
+	const animationClassesMobile: Record<FocusElementType, string> = {
+		name: classes.nameFocusMobile,
+		email: classes.emailFocusMobile,
+		message: classes.messageFocusMobile,
+		"":""
+	};
 
 	return (
 		<div className={classes.container} id='contacts'>
@@ -20,21 +35,21 @@ export const Contacts:FC = () => {
 			<Typography className={classes.description} block variant='body'>I’m interested in freelance opportunities – especially ambitious or large projects. However, if you have other request or question, don’t hesitate to use the form.</Typography>
 			<div className={classes.contentContainer}>
 				<div className={classes.formContainer}>
-					<Track className={cx(classes.animationTrack, isButtonFocus ? classes.buttonHover :  focus) }/>
-					<TrackMobile className={classes.animationTrackMobile}/>
+					<Track className={cx(classes.animationTrack, isButtonFocus ? classes.buttonHover :  animationClasses[focus]) }/>
+					<TrackMobile className={cx(classes.animationTrackMobile, isButtonFocus ? classes.buttonHoverMobile : animationClassesMobile[focus])}/>
 					<div className={classes.infoContainer}>
 						<Input
 							value={name}
 							onChange={setName}
 							label='Name'
-							onFocus={() => setFocus(classes.nameTrackFocus)}
+							onFocus={() => setFocus("name")}
 							className={classes.topInput}
 						/>
 						<Input value={email}
 							onChange={setEmail}
 							label='Email'
 							type='email'
-							onFocus={() => setFocus(classes.emailFocus)}
+							onFocus={() => setFocus("email")}
 							className={classes.topInput}
 						/>
 					</div>
@@ -42,7 +57,7 @@ export const Contacts:FC = () => {
 						value={message}
 						onChange={setMessage}
 						label='Message'
-						onFocus={() => setFocus(classes.messageFocus)}
+						onFocus={() => setFocus("message")}
 						className={classes.bottomInput}
 					/>
 					<Button
