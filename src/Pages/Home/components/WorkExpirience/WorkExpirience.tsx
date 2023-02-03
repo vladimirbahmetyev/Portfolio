145c793b0689dabbb2ef31dac89e6d54609e6d6d
Vladimir <bahv-240298@mail.ru>
2023-02-03 14:39:53 +0400

    Convert packs to webp format

--- src/Pages/Home/components/WorkExpirience/WorkExpirience.tsx
+++ src/Pages/Home/components/WorkExpirience/WorkExpirience.tsx
@@ -33,7 +33,7 @@ export const WorkExpirience:FC= () => {
 				<div className={classes.secondProjectContainer} onClick={()=>onProjectClick("Jobswidget")}>
 					<N2/>
 					<div className={classes.projectContainer}>
-						<img src='/images/workExperience/p2.png' className={classes.secondProjectPack}/>
+						<img src='/images/workExperience/p2.webp' className={classes.secondProjectPack}/>
 						<img src='/images/workExperience/pr2.jpg' className={ classes.secondProjectImage}/>
 						<Typography variant='h3' block>JobsWidget</Typography>
 					</div>
@@ -41,7 +41,7 @@ export const WorkExpirience:FC= () => {
 				<div className={classes.firstProjectContainer  } onClick={()=>onProjectClick("Metamap")}>
 					<N1/>
 					<div className={classes.projectContainer}>
-						<img src='/images/workExperience/p1.png' className={classes.firstProjectPack}/>
+						<img src='/images/workExperience/p1.webp' className={classes.firstProjectPack}/>
 						<img src='/images/workExperience/pr1.jpg'className={classes.firstProjectImage}/>
 						<Typography variant='h3' block>Metamap</Typography>
 					</div>
@@ -50,7 +50,7 @@ export const WorkExpirience:FC= () => {
 			<div className={classes.thirdProjectContainer} onClick={()=>onProjectClick("GameGreed")}>
 				<N3/>
 				<div className={classes.projectContainer}>
-					<img src='/images/workExperience/p3.png' className={classes.thirdProjectPack}/>
+					<img src='/images/workExperience/p3.webp' className={classes.thirdProjectPack}/>
 					<img src='/images/workExperience/pr3.jpg' className={classes.thirdProjectImage}/>
 					<Typography variant='h3' block>Game Greed</Typography>
 				</div>
@@ -58,7 +58,7 @@ export const WorkExpirience:FC= () => {
 			<div className={classes.fourthProjectContainer} onClick={()=>onProjectClick("Mugler")}>
 				<N4/>
 				<div className={classes.projectContainer}>
-					<img src='/images/workExperience/p4.png' className={classes.fourthProjectPack}/>
+					<img src='/images/workExperience/p4.webp' className={classes.fourthProjectPack}/>
 					<img src='/images/workExperience/pr4.jpg'className={classes.fourthProjectImage}/>
 					<Typography variant='h3' block>Mugler</Typography>
 				</div>
@@ -66,7 +66,7 @@ export const WorkExpirience:FC= () => {
 			<div className={classes.fifthProjectContainer} onClick={()=>onProjectClick("MyGlo")}>
 				<N5/>
 				<div className={classes.projectContainer}>
-					<img src='/images/workExperience/p5.png' className={classes.fifthProjectPack}/>
+					<img src='/images/workExperience/p5.webp' className={classes.fifthProjectPack}/>
 					<img src='/images/workExperience/pr5.jpg'className={classes.fifthProjectImage}/>
 					<Typography variant='h3' block>MyGlo</Typography>
 				</div>
@@ -74,7 +74,7 @@ export const WorkExpirience:FC= () => {
 			<div className={classes.sixthProjectContainer} onClick={()=>onProjectClick("CorpPass")}>
 				<N6/>
 				<div className={classes.projectContainer}>
-					<img src='/images/workExperience/p6.png' className={classes.sixthProjectPack}/>
+					<img src='/images/workExperience/p6.webp' className={classes.sixthProjectPack}/>
 					<img src='/images/workExperience/pr6.jpg'className={classes.sixthProjectImage}/>
 					<Typography variant='h3' block>CorpPass</Typography>
 				</div>
@@ -82,7 +82,7 @@ export const WorkExpirience:FC= () => {
 			<div className={classes.seventhProjectContainer} onClick={()=>onProjectClick("Shopic")}>
 				<N7/>
 				<div className={classes.projectContainer}>
-					<img src='/images/workExperience/p7.png' className={classes.seventhProjectPack}/>
+					<img src='/images/workExperience/p7.webp' className={classes.seventhProjectPack}/>
 					<img src='/images/workExperience/pr7.jpg'className={classes.seventhProjectImage}/>
 					<Typography variant='h3' block>Shopic</Typography>
 				</div>
